4bde9bdef656d30037afb6de4d2660d5bb6dd6f2
Feisal Ahmed <feisala132@gmail.com>
2020-09-14 14:33:43 +0100

    modified:   app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -61,7 +61,7 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-     user = User.query.filter_by(username=username).first_or_404()
+    user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
